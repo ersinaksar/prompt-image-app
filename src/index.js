55582447de936af15b061adda91e2635fe92b58c
Ersin Aksar <ersinaksar@yandex.com>
2024-12-22 07:49:24 +0300

    Add GOOGLE_ANALYTICS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
+import ReactGA from "react-ga4";
+
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);// Replace with your Tracking ID from Google Analytics
+ReactGA.send("pageview");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
